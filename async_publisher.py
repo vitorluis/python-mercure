83b40d8d29753f7fa3c290b61a5e9523db7b6c8c
Vitor Villar <v.villar@theplayerdatabase.com>
2019-07-29 00:15:17 +0200

    Publishing message using the sync publisher

---
+++ async_publisher.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Vitor Villar <vitor.luis98@gmail.com>
+"""
+
+
+class AsyncPublisher:
+
+    def __init__(self, mercure_hub, jwt):
+        pass
+
+    async def publish(self, topics, data):
+        pass
---
+++ publisher.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Vitor Villar <vitor.luis98@gmail.com>
+"""
+import urllib
+
+import requests
+
+
+class Publisher:
+    mercure_hub = None
+    mercure_jwt = None
+
+    def __init__(self, mercure_hub, mercure_jwt):
+        self.mercure_hub = mercure_hub
+        self.mercure_jwt = mercure_jwt
+
+    def publish(self, topics: list, data: str) -> str:
+        """
+        Publishes an update to Mercure
+
+        :param list topics: Topics to be published
+        :param str data: The Data itself
+        :return str: The message ID on mercure on UUID format
+
+        """
+        # it has to have at least one topic
+        if type(topics) is not list:
+            raise TypeError('topics must be a list')
+        elif len(topics) == 0:
+            raise AttributeError('topics cannot be empty')
+
+        # And data must be a string
+        if type(data) is not str:
+            raise TypeError('data must be a str')
+
+        # Create the request
+        headers = {
+            'Authorization': 'Bearer ' + self.mercure_jwt,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        form_data = {
+            'topic': topics,
+            'data': data
+        }
+
+        form_data = urllib.parse.urlencode(form_data, True)
+        response = requests.post(self.mercure_hub, form_data, headers=headers)
+
+        return str(response.text)
