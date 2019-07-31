1368e510cf57315b177c8a64b0d3d39e00743db3
Vitor Villar <vitor-luis.villar-neto@sixt.com>
2019-07-31 18:00:41 +0200

    Start to create the consumer

--- consumer.py
+++ consumer.py
@@ -2,28 +2,26 @@
 """
 @author: Vitor Villar <vitor.luis98@gmail.com>
 """
+import requests
+import sseclient
 
 
 class Consumer:
     mercure_hub = None
     topics = None
+    sse_client = None
 
     def __init__(self, mercure_hub, topics):
         self.mercure_hub = mercure_hub
         self.topics = topics
 
-    def start(self):
-        import json
-        import pprint
-        import sseclient
-
-        def with_requests(url):
-            """Get a streaming response for the given event feed using requests."""
-            import requests
-            return requests.get(url, stream=True)
-
+    def start_consumption(self, callback):
         url = 'http://127.0.0.1:3000/hub?topic=mytopicname'
-        response = with_requests(url)  # or with_requests(url)
-        client = sseclient.SSEClient(response)
-        for event in client.events():
-            pprint.pprint(json.loads(event.data))
+        response = self._create_request()
+        self.sse_client = sseclient.SSEClient(response)
+
+        for event in self.sse_client.events():
+            callback(event)
+
+    def _create_request(self):
+        return requests.get(self.mercure_hub, stream=True)
