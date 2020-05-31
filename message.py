4f3a7cfb16013bd0089e39d08ab8dc3e19f5fbb5
Kévin Dunglas <dunglas@gmail.com>
2020-05-31 22:28:37 +0200

    feat: compatibility with Mercure 0.10

--- message.py
+++ message.py
@@ -6,26 +6,26 @@
 
 class Message:
     data = None
-    target = None
+    private = False
     message_id = None
     event_type = None
     retry = None
     topics = None
 
-    def __init__(self, topics, data, target=None, message_id=None, event_type=None, retry=None):
+    def __init__(self, topics, data, private=False, message_id=None, event_type=None, retry=None):
         """
         Defines a message
 
         :param list topics:  Which topics this message will be published
         :param str data: Data itself
-        :param str target: Target audience of this update. This key can be present several times. (Optional)
+        :param str private: To mark this update as private. (Optional)
         :param str message_id: The topic's revision identifier: it will be used as the SSE's id property (Optional)
         :param str event_type: The SSE's event property (a specific event type) (Optional)
         :param int retry:  The SSE's retry property (the reconnection time) (Optional)
         """
         self.topics = topics
         self.data = data
-        self.target = target
+        self.private = private
         self.message_id = message_id
         self.event_type = event_type
         self.retry = retry
